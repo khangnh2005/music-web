d4825ac59cd1d873da6a2ac9b577c47e926b2d42
Khang Nguyễn <dh52300789@student.stu.edu.vn>
2026-08-06 21:26:52 +0700

    Tao routes

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv"
 import * as database from "./config/database"
 import Topic from "./models/topic.model";
+import clientRoutes from "./routes/client/index.route";
 
 
 const app: Express = express();
@@ -20,13 +21,9 @@ dotenv.config()
 database.connect()
 //database connect END
 
-app.get("/topics", async (req: Request, res: Response) => {
-  const topic = await Topic.find({
-    deleted : false
-  })
-  console.log(topic)
-  res.render("client/pages/topics/index")
-});
+//route
+clientRoutes(app)
+//route End
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
