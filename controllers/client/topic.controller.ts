7d9a77f8189dd70e7e334ec1f902a3b45303c0e8
Khang Nguyễn <dh52300789@student.stu.edu.vn>
2026-08-08 15:47:00 +0700

    Cau hinh lai file Pug

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -6,5 +6,7 @@ export const index = async (req : Request ,res : Response) =>{
     deleted: false
   });
   console.log(topics)
-  res.render("client/pages/topics/index");
+  res.render("client/pages/topics/index",{
+    titlePage : "Trang chủ đề"
+  });
 }
