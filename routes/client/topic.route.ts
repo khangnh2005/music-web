91fc3ea7d1862fec20ce6606ae517c44d6701843
Khang Nguyễn <dh52300789@student.stu.edu.vn>
2026-08-08 14:12:58 +0700

    Tao controllers

--- routes/client/topic.route.ts
+++ routes/client/topic.route.ts
@@ -1,16 +1,7 @@
-import { Request, Response, Router } from "express";
+import {  Router } from "express";
+import * as Controller from "../../controllers/client/topic.controller"
 const router: Router = Router();
 
-import Topic from "../../models/topic.model";
-
-router.get("/", async (req: Request, res: Response) => {
-  const topics = await Topic.find({
-    deleted: false
-  });
-
-  console.log(topics);
-
-  res.render("client/pages/topics/index");
-});
+router.get("/", Controller.index);
 
 export const topicRoutes: Router = router; 
