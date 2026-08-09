c2511f2a63fa4424f8b4790333e2ab6ffa20b655
Khang Nguyễn <dh52300789@student.stu.edu.vn>
2026-08-09 17:56:27 +0700

    Lam trang Chu de bai hat(topics)

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -5,8 +5,8 @@ export const index = async (req : Request ,res : Response) =>{
   const topics = await Topic.find({
     deleted: false
   });
-  console.log(topics)
   res.render("client/pages/topics/index",{
-    titlePage : "Trang chủ đề"
+    titlePage : "Trang chủ đề",
+    topics : topics
   });
 }
